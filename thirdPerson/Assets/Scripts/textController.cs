428f3290f919a058924adf4d9d32fed1317f5cba
Marc Gutierrez <zerreitugcram200@gmail.com>
2020-10-06 00:22:54 -0500

    created contextual message script controller
    WIP

---
+++ thirdPerson/Assets/Scripts/textController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class textController : MonoBehaviour
+{
+    private CanvasGroup canvasGroup;
+    private TMP_Text messageText;
+
+    void Start()
+    {
+
+        canvasGroup = GetComponent<CanvasGroup>();
+        messageText = GetComponent<TMP_Text>();
+
+        canvasGroup.alpha = 0;
+    }
+
+    private void ShowMessage(string message, float deration)
+    {
+        messageText.text = message;
+
+    }
+}
