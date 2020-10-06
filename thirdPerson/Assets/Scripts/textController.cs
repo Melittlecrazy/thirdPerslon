f66b593b90e73f701f2dadc2ac2a4c3299b9ceea
Marc Gutierrez <zerreitugcram200@gmail.com>
2020-10-06 01:38:57 -0500

    Added a trigger to give messeges on screen

--- thirdPerson/Assets/Scripts/textController.cs
+++ thirdPerson/Assets/Scripts/textController.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class textController : MonoBehaviour
 {
+    [SerializeField]
+    private float fade;
     private CanvasGroup canvasGroup;
     private TMP_Text messageText;
 
@@ -15,11 +16,39 @@ public class textController : MonoBehaviour
         messageText = GetComponent<TMP_Text>();
 
         canvasGroup.alpha = 0;
+
     }
 
-    private void ShowMessage(string message, float deration)
+    private IEnumerator ShowMessage(string message, float duration)
     {
+        canvasGroup.alpha = 1;
         messageText.text = message;
+        //wait for duration
+        yield return new WaitForSeconds(duration);
+        //start to fage out
+        float elapse = 0;
+        float fadeStart = Time.time;
+        while (elapse<fade)
+        {
+            elapse = Time.time - fadeStart;
+            canvasGroup.alpha = 1 - elapse / fade;
+            yield return null;
+        }
+        canvasGroup.alpha = 0;
+    }
 
+    private void OnMessageTriggered(string message, float messageDuration)
+    {
+        StopAllCoroutines();
+        StartCoroutine(ShowMessage("Honk", 2));
+    }
+    private void OnEnable()
+    {
+        messageTrigger.MessagedTriggered += OnMessageTriggered;
+    }
+
+    private void OnDisable()
+    {
+        messageTrigger.MessagedTriggered -= OnMessageTriggered;
     }
 }
